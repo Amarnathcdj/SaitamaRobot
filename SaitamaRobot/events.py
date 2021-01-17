3237fb429c715b8df9af3735e0f40d11af120a9e
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-01-17 20:16:09 +0530

    Create events.py

---
+++ SaitamaRobot/events.py
@@ -0,0 +1,16 @@
+from SaitamaRobot import CMD_LIST
+import inspect
+
+def register(**args):
+    pattern = args.get("pattern")
+    r_pattern = r"^[/]"
+
+    if pattern is not None and not pattern.startswith("(?i)"):
+        args["pattern"] = "(?i)" + pattern
+
+    args["pattern"] = pattern.replace("^/", r_pattern, 1)
+    stack = inspect.stack()
+    previous_stack_frame = stack[1]
+    file_test = Path(previous_stack_frame.filename)
+    file_test = file_test.stem.replace(".py", "")
+    reg = re.compile("(.*)")
