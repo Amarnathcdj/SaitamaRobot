6553f8598887fddd86ab116c360c7b726f4b3328
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-01-17 22:06:28 +0530

    Update updater.py
    Nz

--- SaitamaRobot/modules/updater.py
+++ SaitamaRobot/modules/updater.py
@@ -1,140 +1,121 @@
-from SaitamaRobot.events import register
-from os import remove, execle, path, environ
-import asyncio
-import sys
+#"""Update UserBot Code (FOR DARKCOBRA USERBOT)
+#Syntax: .update
+#\nAll Credits goes to © @hellboi_atul
+#\nFor this awasome plugin.\nPorted from PpaperPlane Extended"""
 from telethon import events
 from os import remove, execle, path, makedirs, getenv, environ, execl
 from shutil import rmtree
+import asyncio
+import sys
 from git import Repo
 from git.exc import GitCommandError, InvalidGitRepositoryError, NoSuchPathError
-import heroku3
-from SaitamaRobot import OWNER_ID, tbot
+from SaitamaRobot.utils import admin_cmd
 
-
-UPSTREAM_REPO_URL = "https://github.com/Amarnathcdj/SaitamaRobot.git"
-HEROKU_APP_NAME = None
-HEROKU_API_KEY = None
+UPSTREAM_REPO_URL = "https://github.com/DARK-COBRA/DARKCOBRA"
+HEROKU_API_KEY = Var.HEROKU_API_KEY
+HEROKU_APP_NAME = Var.HEROKU_APP_NAME
 
 requirements_path = path.join(
-    path.dirname(path.dirname(path.dirname(__file__))), "requirements.txt"
-)
-
+    path.dirname(path.dirname(path.dirname(__file__))), 'requirements.txt')
 
 async def gen_chlog(repo, diff):
-    ch_log = ""
+    ch_log = ''
     d_form = "%d/%m/%y"
     for c in repo.iter_commits(diff):
-        ch_log += (
-            f"•[{c.committed_datetime.strftime(d_form)}]: {c.summary} by <{c.author}>\n"
-        )
+        ch_log += f'•[{c.committed_datetime.strftime(d_form)}]: {c.summary} by <{c.author}>\n'
     return ch_log
 
-
-async def updateme_requirements():
+async def update_requirements():
     reqs = str(requirements_path)
     try:
         process = await asyncio.create_subprocess_shell(
-            " ".join([sys.executable, "-m", "pip", "install", "-r", reqs]),
+            ' '.join([sys.executable, "-m", "pip", "install", "-r", reqs]),
             stdout=asyncio.subprocess.PIPE,
-            stderr=asyncio.subprocess.PIPE,
-        )
+            stderr=asyncio.subprocess.PIPE)
         await process.communicate()
         return process.returncode
     except Exception as e:
         return repr(e)
 
-def leave(update: Update, context: CallbackContext):
-    lol = await ups.reply("`Checking for updates, please wait....`")
+@borg.on(admin_cmd(pattern="update ?(.*)", outgoing=True))
+async def upstream(ups):
+    "For .update command, check if the bot is up to date, update if specified"
     conf = ups.pattern_match.group(1)
+    await ups.edit("Checking for updates, please wait....")
     off_repo = UPSTREAM_REPO_URL
     force_update = False
-
     try:
-        txt = "`Oops.. Updater cannot continue "
-        txt += "please add heroku apikey, name`\n\n**LOGTRACE:**\n"
+        txt = "Oops.. Updater cannot continue due to "
+        txt += "some problems occured`\n\n**LOGTRACE:**\n"
         repo = Repo()
     except NoSuchPathError as error:
-        await lol.edit(f"{txt}\n`directory {error} is not found`")
+        await ups.edit(f'{txt}\ndirectory {error} is not found')
         repo.__del__()
         return
     except GitCommandError as error:
-        await lol.edit(f"{txt}\n`Early failure! {error}`")
+        await ups.edit(f'{txt}\nEarly failure! {error}')
         repo.__del__()
         return
     except InvalidGitRepositoryError as error:
         if conf != "now":
-            await lol.edit(
-                f"**Unfortunately, the directory {error} does not seem to be a git repository.\
-            \nBut we can fix that by force updating the bot using** `/update now`"
-            )
+            await ups.edit(f"**Hey ßoss!!!**😁😁\n__To get the Latest update of__ \n©DARK_COBRA_SUPPORT\n\n do |`.update now`| 😎😎 ")
             return
         repo = Repo.init()
-        origin = repo.create_remote("upstream", off_repo)
+        origin = repo.create_remote('upstream', off_repo)
         origin.fetch()
         force_update = True
-        repo.create_head("master", origin.refs.master)
+        repo.create_head('master', origin.refs.master)
         repo.heads.master.set_tracking_branch(origin.refs.master)
         repo.heads.master.checkout(True)
-
     ac_br = repo.active_branch.name
-    if ac_br != "master":
-        await lol.edit(
-            f"**[UPDATER]:**` Looks like you are using your own custom branch ({ac_br}). "
-            "in that case, Updater is unable to identify "
-            "which branch is to be merged. "
-            "please checkout to any official branch`"
-        )
+    if ac_br != 'master':
+        await ups.edit(
+            f'**[UPDATER]:**` Looks like you are using your own custom branch ({ac_br}). '
+            'in that case, Updater is unable to identify '
+            'which branch is to be merged. '
+            'please checkout to any official branch`')
         repo.__del__()
         return
-
     try:
-        repo.create_remote("upstream", off_repo)
+        repo.create_remote('upstream', off_repo)
     except BaseException:
         pass
-
-    ups_rem = repo.remote("upstream")
+    ups_rem = repo.remote('upstream')
     ups_rem.fetch(ac_br)
-
-    changelog = await gen_chlog(repo, f"HEAD..upstream/{ac_br}")
-
+    changelog = await gen_chlog(repo, f'HEAD..upstream/{ac_br}')
     if not changelog and not force_update:
-        await lol.edit("\n`Your bot is`  **up-to-date**  \n")
+        await ups.edit(
+            f'\n**{ac_br} master your bot is already up to date..**\n')
         repo.__del__()
         return
-
     if conf != "now" and not force_update:
-        changelog_str = (
-            f"**New UPDATE available for {ac_br}\n\nCHANGELOG:**\n`{changelog}`"
-        )
+        changelog_str = f'**New UPDATE available for [{ac_br}]:\n\nCHANGELOG:**\n`{changelog}`'
         if len(changelog_str) > 4096:
-            await lol.edit("`Changelog is too big, view the file to see it.`")
+            await ups.edit("`Changelog is too big, view the file to see it.`")
             file = open("output.txt", "w+")
             file.write(changelog_str)
             file.close()
-            await tbot.send_file(
+            await ups.client.send_file(
                 ups.chat_id,
                 "output.txt",
                 reply_to=ups.id,
             )
             remove("output.txt")
         else:
-            await lol.edit(changelog_str)
-        await ups.respond("**do** `/update now` **to update**")
+            await ups.edit(changelog_str)
+        await ups.respond("do `.update now` to update")
         return
-
     if force_update:
-        await lol.edit("`Force-Syncing to latest master bot code, please wait...`")
+        await ups.edit('Force-Syncing to latest stable userbot code, please wait master...😅😅')
     else:
-        await lol.edit("`Still Running ....`")
-
+        await ups.edit('`Updating userbot, please wait....you arey best boss🤗😇')
     if HEROKU_API_KEY is not None:
+        import heroku3
         heroku = heroku3.from_key(HEROKU_API_KEY)
         heroku_app = None
         heroku_applications = heroku.apps()
         if not HEROKU_APP_NAME:
-            await lol.edit(
-                "`Please set up the HEROKU_APP_NAME variable to be able to update your bot.`"
-            )
+            await ups.edit('Please set up the `HEROKU_APP_NAME` variable to be able to update userbot.')
             repo.__del__()
             return
         for app in heroku_applications:
@@ -142,39 +123,41 @@ def leave(update: Update, context: CallbackContext):
                 heroku_app = app
                 break
         if heroku_app is None:
-            await lol.edit(
-                f"{txt}\n`Invalid Heroku credentials for updating userbot dyno.`"
+            await ups.edit(
+                f'{txt}\n`Invalid Heroku credentials for updating userbot dyno.`'
             )
             repo.__del__()
             return
-        await lol.edit(
-            "`[Updater]\
-                        Your bot is being deployed, please wait for it to complete.\nIt may take upto 5 minutes `"
-        )
         ups_rem.fetch(ac_br)
         repo.git.reset("--hard", "FETCH_HEAD")
         heroku_git_url = heroku_app.git_url.replace(
-            "https://", "https://api:" + HEROKU_API_KEY + "@"
-        )
+            "https://", "https://api:" + HEROKU_API_KEY + "@")
         if "heroku" in repo.remotes:
             remote = repo.remote("heroku")
             remote.set_url(heroku_git_url)
         else:
             remote = repo.create_remote("heroku", heroku_git_url)
-        try:
-            remote.push(refspec="HEAD:refs/heads/master", force=True)
-        except GitCommandError as error:
-            await lol.edit(f"{txt}\n`Here is the error log:\n{error}`")
-            repo.__del__()
-            return
-        await lol.edit("Successfully Updated!\n" "Restarting.......")
+            await ups.edit("`⬛⬛⬛⬛ \n⬛✳️✳️⬛ \n⬛✳️✳️⬛ \n⬛⬛⬛⬛`")
+            await asyncio.sleep(1)
+            await ups.edit("`⬛⬛⬛⬛ \n⬛🔴🔴⬛ \n⬛🔴🔴⬛ \n⬛⬛⬛⬛`")
+            await asyncio.sleep(1)
+            await ups.edit("`⬛⬛⬛⬛ \n⬛🌕🌕⬛ \n⬛🌕🌕⬛ \n⬛⬛⬛⬛`")
+            await asyncio.sleep(1)
+            await ups.edit("`⬛⬛⬛⬛ \n⬛🔵🔵⬛ \n⬛🔵🔵⬛ \n⬛⬛⬛⬛`")
+            await asyncio.sleep(1)
+            await ups.edit("`⬛⬛⬛⬛ \n⬛❇️❇️⬛ \n⬛❇️❇️⬛ \n⬛⬛⬛⬛`")
+            await asyncio.sleep(1)
+        await ups.edit("`⚜️Updating DarkCobra⚜️\n\nYou are the 👑KING👑 Boss!!\n\nPlease wait 5min😁😁\nThen try .alive to check` 😎😎\n\n**Powered by :-**\n©DARK_COBRA_SUPPORT ")
+        remote.push(refspec="HEAD:refs/heads/master", force=True)
     else:
         try:
             ups_rem.pull(ac_br)
         except GitCommandError:
             repo.git.reset("--hard", "FETCH_HEAD")
-        reqs_upgrade = await updateme_requirements()
-        await lol.edit("`Successfully Updated!\n" "restarting......`")
-        args = [sys.executable, "-m", "julia"]
+        reqs_upgrade = await update_requirements()
+        await ups.edit('`Successfully Updated!\n'
+                       'Bot is restarting... Wait for a second!`')
+        # Spin a new instance of bot
+        args = [sys.executable, "-m", "userbot"]
         execle(sys.executable, *args, environ)
         return
